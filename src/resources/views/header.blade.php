73ed18189a37ba4b2a9f1713392da66a3875163d
yuyamh <yuya.htr0828@gmail.com>
2022-12-06 17:20:52 +0900

    レイアウトの共通化

---
+++ src/resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">だてまき</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">教案一覧 <span class="sr-only"></span></a>
+            <a class="nav-item nav-link" href="#">教案投稿</a>
+        </div>
+    </div>
+</nav>
