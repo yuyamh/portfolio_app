50086531f5969d0dda994ee9211c5ef5efc38a57
yuyamh <yuya.htr0828@gmail.com>
2022-12-14 13:49:33 +0900

    teaching_plansテーブルにfileカラム（アップロードされた教案を保存）を追加

--- src/database/migrations/2022_11_29_221601_create_teaching_plans_table.php
+++ src/database/migrations/2022_11_29_221601_create_teaching_plans_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
                 $table->id();
                 $table->string('title', 100);
                 $table->text('content');
+                $table->text('file');
                 $table->timestamps();
             });
         }
